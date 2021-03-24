5b927d9708d02441ccfb5350cdf58e913c73f48f
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-03-23 21:29:57 -0400

    working function and updated HTML

--- app.js
+++ app.js
@@ -1,33 +1,42 @@
-//import DOM elements
+import { isYes } from './utils.js';
+
+
 const quizButton = document.getElementById('quiz-button');
 const resultsDivDisplay = document.getElementById('results-div');
 
 
-// create a function for the click button to prompt a quiz
-// button with .addEventListener('click', () => {})
 
 
 quizButton.addEventListener('click', () => {
+    alert('Welcome to The Smiths Quiz');
 
+    const wantsToTakeTheQuiz = confirm('Are you sure you want to take this quiz??');
 
+    if (wantsToTakeTheQuiz === false) {
+        alert('Okay, okay, remember to still listen to The Smiths');
+        return;
+    }
 
+    const name = prompt('What is your name?');
+    let score = 0;
 
-//announce there is a quick
-    //alert('')
-//Does user want to take quiz
-    //confirm('')
-//get users name
-    //prompt('')
-//ask 3 questions
-    //prompt('')
-    //prompt('')
-    //prompt('')
-//score the quiz
-    //isYes('')
-//Display results
-    //.textContent = ${}
+    const firstAnswer = prompt('Are The Smiths from Manchester?');
+    const secondAnswer = prompt('Is Morrissey the drummer of The Smiths?');
+    const thirdAnswer = prompt('Are The Smiths the greatest band of all time?');
 
+    if (isYes(firstAnswer)) {
+        score++;
+    }
 
+    if (!isYes(secondAnswer)) {
+        score++;
+    }
 
+    if (isYes(thirdAnswer)) {
+        score++;        
+    }
 
+    const resultsString = `Hi ${name} you scored a ${score} out of three.`;
+
+    resultsDivDisplay.textContent = resultsString;
 });
